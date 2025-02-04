a00872cbafd312d4aa2f1a2d1e99f7ad7b649a32
kashyap vishnudas <kashyapvishnudas@gmail.com>
2025-02-04 20:01:03 +0530

    new chnage

--- app.py
+++ app.py
@@ -400,5 +400,5 @@ def delete_chit():
 #     except Exception as e:
 #         return jsonify({'error': str(e)}), 400
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
